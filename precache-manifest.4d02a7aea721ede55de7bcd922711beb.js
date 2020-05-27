9eab1ce4a47afc838ddb50a9d60ad60d915122d7
Fukagawa <Michiharu.Fukagawa@sony.com>
2020-05-27 12:44:32 +0900

    Updates

---
+++ precache-manifest.4d02a7aea721ede55de7bcd922711beb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a18f9b457bedf876f711933cb3303d92",
+    "url": "/wolf/index.html"
+  },
+  {
+    "revision": "e0233df370c60ca73a1e",
+    "url": "/wolf/static/js/2.1af83bac.chunk.js"
+  },
+  {
+    "revision": "86c38526793870b25975195299822b16",
+    "url": "/wolf/static/js/2.1af83bac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "935304f21dff7b5dbfcf",
+    "url": "/wolf/static/js/main.dc714459.chunk.js"
+  },
+  {
+    "revision": "e4747bb35ead0233b04e",
+    "url": "/wolf/static/js/runtime-main.8262428f.js"
+  }
+]);
